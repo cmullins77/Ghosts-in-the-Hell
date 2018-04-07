e4d6b54e0682e378a10b85cca8c6aedb440f8ae0
vedantswain <vedant12118@iiitd.ac.in>
2018-04-07 17:57:59 -0400

    added basics to sonic

---
+++ BlackFarmer/Assets/Sonic/Scripts/RingController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// Increase the score everytime you scare pacman
+		if(other.name == "cart")
+		{
+			// GameManager.score += 100;
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ BlackFarmer/Assets/Sonic/Scripts/RingSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingSpawner : MonoBehaviour {
+
+	public GameObject ring;
+
+	private float radius = 14f;
+	private float centre_x = 0;
+	private float centre_y = 0;
+
+	private float spawn_interval = 5f;
+	private float time_elapsed = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time_elapsed += Time.deltaTime;
+
+		if(time_elapsed > spawn_interval){
+			float angle = 2 * Mathf.PI * Random.value;
+			float x = Mathf.Cos(angle)*radius;
+			float y = Mathf.Sin(angle)*radius;
+
+			Vector2 ring_position = new Vector2(x,y);
+
+			Instantiate(ring, ring_position, transform.rotation);
+
+			time_elapsed = 0;
+		}
+	}
+}
