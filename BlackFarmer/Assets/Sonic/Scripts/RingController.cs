119c736b2b55650dc4c9328df42aea297ab777d0
vedantswain <vedant12118@iiitd.ac.in>
2018-04-08 03:02:36 -0400

    roller coaster sonic 90% done

--- BlackFarmer/Assets/Sonic/Scripts/RingController.cs
+++ BlackFarmer/Assets/Sonic/Scripts/RingController.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class RingController : MonoBehaviour {
 
+	public GameObject ring_spawner;
+
+	private float lo_threshold=0;
+	private float hi_threshold=0;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {	
+
+		if(gameObject.name == "ring_blue(Clone)"){
+			lo_threshold=0;
+			hi_threshold=33;
+		}
+		if(gameObject.name == "ring_yellow(Clone)"){
+			lo_threshold=33;
+			hi_threshold=66;
+		}
+		if(gameObject.name == "ring_red(Clone)"){
+			lo_threshold=66;
+			hi_threshold=100;
+		}
 	}
 	
 	// Update is called once per frame
@@ -20,7 +37,13 @@ public class RingController : MonoBehaviour {
 		if(other.name == "cart")
 		{
 			// GameManager.score += 100;
-			Destroy(gameObject);
+			float cartVelocity = other.GetComponent<Rigidbody2D>().velocity.magnitude;
+			if(cartVelocity>lo_threshold && cartVelocity<hi_threshold){
+				// print("lo: "+lo_threshold+", hi: "+hi_threshold+", vel: "+cartVelocity);
+				RingSpawner.ring_count -= 1;
+				ring_spawner.GetComponent<RingSpawner>().KillPing();
+				Destroy(gameObject);	
+			}
 		}
 	}
 }
---
+++ BlackFarmer/Assets/Sonic/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	private Text scores_txt;
+	private Text time_txt;
+	private Text speed_txt;
+	private GameObject cart;
+
+	private float timeElapsed=0;
+	private float score=0;
+
+
+	// Use this for initialization
+	void Start () {
+		scores_txt = GameObject.Find("/CanvasHUD/Stats/Player Score").GetComponent<Text>();
+		speed_txt = GameObject.Find("/CanvasHUD/Stats/Speed").GetComponent<Text>();
+		time_txt = GameObject.Find("/CanvasHUD/Stats/Time").GetComponent<Text>();
+		cart = GameObject.Find("cart");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		timeElapsed += Time.deltaTime;
+		string time_string = "Time\n" + Mathf.Round(timeElapsed);
+		time_txt.text = time_string;
+
+		float cartVelocity = cart.GetComponent<Rigidbody2D>().velocity.magnitude;
+		cartVelocity = Mathf.Round(cartVelocity);
+		string speed_string = "Speed\n" + cartVelocity;
+		speed_txt.text = speed_string;
+
+		if(cartVelocity>0 && cartVelocity<=33){
+			speed_txt.color = Color.blue;
+		}
+		if(cartVelocity>33 && cartVelocity<=66){
+			speed_txt.color = Color.yellow;
+		}
+		if(cartVelocity>66 && cartVelocity<=100){
+			speed_txt.color = Color.red;
+		}
+	}
+
+	public void UpdateScore (float add_score) {
+		score += add_score;
+		string score_string = "Score\n" + score;
+		scores_txt.text = score_string;
+	}
+}
