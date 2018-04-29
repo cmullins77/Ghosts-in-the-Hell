8f3e39660785ae2123377580cb4c7b7e2dbd5a64
cmullins77 <mullins.cassie@gmail.com>
2018-04-29 01:34:42 -0400

    Added Main Menu & Tutorial

---
+++ Grindhogs-Day/Assets/TutorialCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialCamera : MonoBehaviour {
+    public GameObject player;
+    public float max;
+    public float min;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float playerPos = player.transform.position.x;
+        if (playerPos < min) {
+            transform.position = new Vector3(min, 0, -10);
+        } else if (playerPos > max) {
+            transform.position = new Vector3(max, 0, -10);
+        } else {
+            transform.position = new Vector3(playerPos, 0, -10);
+        }
+        if (playerPos > 11.7) {
+            SceneManager.LoadScene(0);
+        }
+	}
+}
---
+++ Grindhogs-Day/Assets/TutorialNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialNode : MonoBehaviour {
+    public Color deactiveColor;
+    public Color activeColor;
+    public GameObject tutorialText;
+    public GameObject player;
+
+    public float x;
+    public float y;
+	// Use this for initialization
+	void Start () {
+        x = transform.position.x;
+        y = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float pX = player.transform.position.x;
+        float pY = player.transform.position.y;
+        if (pX <= x + 0.4 && pX >= x - 0.4 && pY <= 0.8 + y && pY >= y - 0.8) {
+            tutorialText.SetActive(true);
+            GetComponent<SpriteRenderer>().color = activeColor;
+        } else {
+            tutorialText.SetActive(false);
+            GetComponent<SpriteRenderer>().color = deactiveColor;
+        }
+	}
+}
